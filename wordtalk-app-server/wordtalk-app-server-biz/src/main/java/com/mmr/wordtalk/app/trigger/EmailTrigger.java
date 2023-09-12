e3f22a90b4672d48a858d24911e732718b6b8a16
张恩睿 <1440565153@qq.com>
2023-09-12 11:31:00 +0800

    重新创建app模块

--- wordtalk-app-server/wordtalk-app-server-biz/src/main/java/com/mmr/wordtalk/app/trigger/EmailTrigger.java
+++ wordtalk-app-server/wordtalk-app-server-biz/src/main/java/com/mmr/wordtalk/app/trigger/EmailTrigger.java
@@ -2,6 +2,7 @@ package com.mmr.wordtalk.app.trigger;
 
 import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class EmailTrigger {
 
     @Lazy
+    @Autowired
     private MailSender mailSender;
 
     // 注册时向传入的邮箱发送验证码的方法
